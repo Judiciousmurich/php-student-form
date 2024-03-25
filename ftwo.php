636c460623bb70b59d5e9384c232949b1e09817a
judiciousmurich <murichjudicious45@gmail.com>
2024-03-25 21:21:02 +0300

    feat:add php

--- ftwo.php
+++ ftwo.php
@@ -1,2 +1,10 @@
 <?php
-$admno = $_POST['sadmno']
+$sadmno = $_POST['sadmno'];
+$sname = $_POST[ 'sname' ];
+$con = new mysqli("localhost", "", "kirinyaga");
+if(!$con)
+{
+    echo "COULD NOT CONNECT TO THE DATABASE";
+}
+$sql = "INSERT INTO student VALUES('$sadmno', '$name')";
+$result = SCON-
