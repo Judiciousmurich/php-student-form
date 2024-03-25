c8ba05cb2d666d0d8a92006b618a8996b862a530
judiciousmurich <murichjudicious45@gmail.com>
2024-03-25 23:09:06 +0300

    feat:add processor.php

--- sample.php
+++ sample.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
-<body>
+<body> 
 <form method="post" action="processor.php"> 
 <input type="submit" value="DISPLAY" /> 
-</form>
+</form> 
 </body>
+
 </html>
